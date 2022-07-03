137da8b41684788b80467d24a1e56c1ce56db5ee
Primitive J <hokecj@gmail.com>
2022-07-03 16:35:04 -0400

    created db.js to handle storage/cache logic. Updated index.js to contain a delete method.

---
+++ db.js
@@ -0,0 +1,65 @@
+let db;
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  // Check status of application (online) and runs checkDatabase() method. 
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function (event) {
+  console.log("Woops! " + event.target.errorCode);
+};
+
+// method that takes a record and stores a pending transaction to be written 
+function saveRecord(record) {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+
+  store.add(record);
+}
+
+//method to run when online to check database and store cached changes via POST when online
+function checkDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+        .then(response => response.json())
+        .then(() => {
+          // delete records if successful
+          const transaction = db.transaction(["pending"], "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
+    }
+  };
+}
+
+//Delete cached records 
+function deletePending() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  store.clear();
+}
+
+// Listen for changes to online status. 
+window.addEventListener("online", checkDatabase);
